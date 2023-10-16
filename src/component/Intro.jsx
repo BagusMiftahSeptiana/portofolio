8eef9d3942aace86d4658f599a4f15c600681d6f
BagusMiftahSeptiana <144077995+BagusMiftahSeptiana@users.noreply.github.com>
2023-10-16 11:53:21 +0700

    Add files via upload

---
+++ src/component/Intro.jsx
@@ -0,0 +1,24 @@
+function Intro() {
+    return(
+        <div className="section text-light">
+            <div className="container text-center">
+                <img
+                className="avatar"
+                src="https://somireddylaw.com/wp-content/uploads/2022/01/fdamap-testimonial-male-avatar.png"
+                />
+                <h1>Bagus Miftah Septiana</h1>
+                <p>Web Developer</p>
+                <a target="_blank" href="https://www.facebook.com/bagusmiftah.septiana" className="social-media">
+                    <i class="bi bi-facebook"></i>
+                </a>
+                <a target="_blank" href="https://instagram.com/_mfth89?igshid=YTQwZjQ0NmI0OA==" className="social-media">
+                    <i class="bi bi-instagram"></i>
+                </a>
+                <a target="_blank" href="https://github.com/BagusMiftahSeptiana" className="social-media">
+                    <i class="bi bi-github"></i>
+                </a>
+            </div>
+        </div>
+    )
+}
+export default Intro
---
+++ src/component/RecentWork.jsx
@@ -0,0 +1,36 @@
+function RecentWork() {
+    return(
+        <div className="section bg-dark text-light">
+            <div className="container">
+                <h1>RecentWork Components</h1>
+                <div className="row mt-5">
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                    <div className="col-4 mt-4">
+                        <img className="img-fluid" src="https://wallpapercave.com/wp/wp4288365.jpg"
+                        />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+export default RecentWork
